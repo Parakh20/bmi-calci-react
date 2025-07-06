bb325b2ae0b7159be32a7cc02763be0dce1094d7
[Parakh <[sharmaparakh05@gmail.com]>
2025-07-06 19:59:17 +0530

    files updated

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import BMIApp from './App'
+
+export const meta = {
+  title: 'BMI Calculator',
+  author: 'Parakh Sharma',
+  description: 'A Minimalistic BMI Calculator built with React',
+  thumbnail: '/bmi.png'
+}
+
+export default BMIApp
